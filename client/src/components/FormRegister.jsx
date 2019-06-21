fedbcd085fe2e4e025fa3ee9c3318bfd3d0d7925
Marie-AnneS <marie-anne.seim@gmail.com>
2019-06-21 18:26:01 +0000

    mistake

---
+++ client/src/components/FormRegister.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FormRegister = () => {
+  return (
+    <div>
+      <form action="">
+        
+      </form>
+    </div>
+  );
+};
+
+export default FormRegister;
