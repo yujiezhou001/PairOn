3dd43981b397e95baa2e984f4b78dc6c99128a9d
RockstarSteph <stephaniechreifi@gmail.com>
2019-06-21 19:01:03 +0000

    added query to db in app component

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,28 +3,53 @@ import axios from 'axios';
 
 class Home extends React.Component {
   //sets intial state
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     currentUser: {firstName: null, hometown: null, experiences: "All", avatarURL: null, currentLocation: { lat, lon, "IE. GOOGLE API"}, aboutMe: null}
-  //     clientList: [], // full of currentUser objects sent from WebSocket
-  //     Chatmessages: []
-  //   };
+  constructor(props){
+    super(props);
+    this.state = {
+      currentUser: {firstName: null, hometown: null, experiences: "All", avatarURL: null, currentLocation: { lat, lng, "IE. GOOGLE API"}, aboutMe: null}
+      clientList: [], // full of currentUser objects sent from WebSocket
+      Chatmessages: []
+    };
   }
 
- //    updateUserList = (newName) => {
- // // const newCurrentUser = Object.assign({}, this.state.currentUser)
- //  // newCurrentUser.name = newName
- //  const newCurrentUser = { ...this.state.currentUser, name: newName, hometown: this.state.currentUser.hometown}
 
- //  const oldName = this.state.currentUser.name;
+getUserInfoFromDB = () => {
+  const userObjFromDB = {};
+  knex
+      .select("*")
+      .from ("users")
+      .where({users.id = user_id})
+      .then(results => {
+        userObjFromDB = results;
+      })
+  return userObjFromDB;
+}
+// should I pass the currentUser Obj?
+    updateUserList = (newuser) => {
+ // const newCurrentUser = Object.assign({}, this.state.currentUser)
+  // newCurrentUser.name = newName
 
- //  this.setState({currentUser: newCurrentUser})
+      const newCurrentUser = { ...this.state.currentUser, firstName: newUser.firstName , hometown: this.state.currentUser.hometown, experiences: "All",}
 
- //  const notificationObj = {
- //    type: "postNotification",
- //    content: ` ${oldName} changed their username to ${newCurrentUser.name}`
- //    }
+
+      this.setState({currentUser: newCurrentUser})
+
+      const notificationObj = {
+        type: "newCurrentUser",
+        content: {} // passing the new user object to push to the client list
+        }
+
+
+this.state = {
+      currentUser: {name: "Anonymous", hometown: ""}
+  changeName = (newName) => {
+ // const newCurrentUser = Object.assign({}, this.state.currentUser)
+  // newCurrentUser.name = newName
+  const newCurrentUser = { ...this.state.currentUser, name: newName, hometown: this.state.currentUser.hometown}
+
+  const oldName = this.state.currentUser.name;
+
+  this.setState({currentUser: newCurrentUser})
 
  //  this.serverSocket.send(JSON.stringify(notificationObj));
  //  //type prop needs to be attached to all objects sent through socket
