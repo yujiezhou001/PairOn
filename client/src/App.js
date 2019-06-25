c9ada79ff99986fc47f56b4e095b5656c8bbfeef
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-06-25 20:39:18 +0000

    Merging master and tested

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.state = {
       currentUser: {
         id: null,
-        firstName: "Chantal",
+        firstName: null,
         hometown: null,
         experiences: "All",
         avatarURL: null,
