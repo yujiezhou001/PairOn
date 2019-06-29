c9ed86730e272e87772efeacfbc2b4974e9a328b
Gordon <gordon@Gordons-MacBook-Air.local>
2019-06-29 13:43:57 -0400

    make it better

---
+++ backend/public/precache-manifest.947452fdadfd86ec99b77f395721f1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724e2bd0a7238cd5e8f33850e73387a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec92ce4fe48f9a1cb76",
+    "url": "/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "417c63ec689c7ff5c61d",
+    "url": "/static/css/main.b3ddcb51.chunk.css"
+  },
+  {
+    "revision": "aec92ce4fe48f9a1cb76",
+    "url": "/static/js/2.e99bfcb6.chunk.js"
+  },
+  {
+    "revision": "417c63ec689c7ff5c61d",
+    "url": "/static/js/main.a02ebe56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
